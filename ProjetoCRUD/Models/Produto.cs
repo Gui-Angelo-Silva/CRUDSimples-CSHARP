0186f20027e7cd5c81b43d6aa00c9e25b33b7e7c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-05-31 16:09:27 -0300

    Commit add

--- ProjetoCRUD/Models/Produto.cs
+++ ProjetoCRUD/Models/Produto.cs
@@ -1,6 +1,19 @@
-﻿namespace ProjetoCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoCRUD.Models
 {
+    [Table ("Produtos")]
     public class Produto
     {
+        [Column ("Id")]
+        [Display (Name = "Id")]
+
+        public int Id { get; set; }
+
+        [Column ("Descricao")]
+        [Display (Name = "Descricao")]
+
+        public string Descricao { get; set; }
     }
 }
